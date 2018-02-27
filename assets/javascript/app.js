877426327a10c3ac2b528033b0df33ee5f858979
Sam <samueljacksonhicks@gmail.com>
2018-02-27 03:19:49 -0600

    code super clean. it's 3:20am and I just sat here cleaning code and adding gifs for 2 and a half hours.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,106 +1,128 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	const startingTime = 20;
 
-	function Query(question, choices, answer){
+	let time = $("#time").text(startingTime);
+	let timeLeft = startingTime;
+	let timeCounter = setInterval(decrementTime, 1000 * 1);
+
+	let queryNum;
+	let question = $("#question");
+	let choices = $("#choices");
+
+	function Query(num, question, choices, answer) {
+
+		this.num = num;
 		this.question = question;
 		this.choices = choices;
 		this.answer = answer;
-	}
 
+	}
 
 	let queries = [];
 
-	queries.push(new Query("(S1E1 'Pilot') What character starts their first day at Dunder Mifflin on the show's Pilot?",
-					 ["Erin Hannon",
-					  "Ryan Howard",
-					  "Jim Halpert",
-					  "Michael Scott"],
+	function randomQuestion() {
 
-					  "Ryan Howard")
-	);
-	queries.push(new Query("(S2E2 'Sexual Harassment') What is Todd Packer's liscense plate?",
-					 ["BGDADY",
-					  "TODPKR",
-					  "LUVMKR",
-					  "WLHUNG"],
-					  
-					  "WLHUNG")
-	);
-	queries.push(new Query("(S2E13 'The Injury') What is Michael's injury?",
-					 ["He gets his head stuck in a stair railing",
-					  "He's run over by a co-worker",
-					  "He crashes his car into a telephone pole",
-					  "He burns his foot on a George Forman Grill"],
-					  
-					  "He burns his foot on a George Forman Grill")
-	);
-	queries.push(new Query("(S2E20 'Drug Testing') Who, in Dwight's mind, is a potential drug mule?",
-					 ["Oscar",
-					  "Creed",
-					  "Jim",
-					  "Merideth"],
-					  
-					  "Oscar")
-	);
-	queries.push(new Query("(S3E8 'The Merger') Who from Stamford quits on their first day in Scranton, only to be fired a minute later?",
-					 ["Hannah",
-					  "Andy",
-					  "Tony",
-					  "Karen"],
-					  
-					  "Tony")
-	);
-	queries.push(new Query("(S3E17 'Cocktails') Who gets stuck in a straight jacket?",
-					 ["Michael",
-					  "Dwight",
-					  "Creed",
-					  "Merideth"],
-					  
-					  "Michael")
-	);
-	/*queries.push(new Query("(S-E- '-') ?",
-						 ["",
-						  "",
-						  "",
-						  ""],
-						  
-						  "")
-		)*/
-
-	function randomQuestion(){
 		let randNum = Math.floor(Math.random() * queries.length);
-		$("#question").text(queries[randNum].question);
-		$("#choices").empty()
-		queries[randNum].choices.forEach(function(value, index){
-			$("#choices").append("<li class='choice' id='" + index + "'>" + value + "</li>");
+		question.text(queries[randNum].question);
+		choices.empty()
+		queries[randNum].choices.forEach(function(value, index) {
+			choices.append("<li class='choice' id='" + index + "'>" + value + "</li>");
 		});
 
-		return randNum;
+		return queries[randNum];
+
 	}
 
-	function decrementTime(){
+	function decrementTime() {
+
 		timeLeft--;
-		$("#time").text(timeLeft);
+		time.text(timeLeft);
+
 	}
 
-	$("#time").text(startingTime);
-	let timeLeft = startingTime;
-	let timeCounter = setInterval( decrementTime, 1000 * 1);
+	queries.push(new Query(0,
+		"(S1E1 'Pilot') What character starts their first day at Dunder Mifflin on the show's Pilot?", [
+			"Erin Hannon",
+			"Ryan Howard",
+			"Jim Halpert",
+			"Michael Scott"
+		],
 
-	let questionNums = [];
-	questionNums.push(randomQuestion());
+		"Ryan Howard"));
+	queries.push(new Query(1,
+		"(S2E2 'Sexual Harassment') What is Todd Packer's liscense plate?", [
+			"BGDADY",
+			"TODPKR",
+			"LUVMKR",
+			"WLHUNG"
+		],
 
-	$("ul").on("click", ".choice", function(){
+		"WLHUNG"));
+	queries.push(new Query(2,
+		"(S2E13 'The Injury') What is Michael's injury?", [
+			"He gets his head stuck in a stair railing",
+			"He's run over by a co-worker",
+			"He crashes his car into a telephone pole",
+			"He burns his foot on a George Forman Grill"
+		],
+
+		"He burns his foot on a George Forman Grill"));
+	queries.push(new Query(3,
+		"(S2E20 'Drug Testing') Who, in Dwight's mind, is a potential drug mule?", [
+			"Oscar",
+			"Creed",
+			"Jim",
+			"Merideth"
+		],
+
+		"Oscar"));
+	queries.push(new Query(4,
+		"(S3E8 'The Merger') Who from Stamford quits on their first day in Scranton, only to be fired a minute later?", [
+			"Hannah",
+			"Andy",
+			"Tony",
+			"Karen"
+		],
+
+		"Tony"));
+	queries.push(new Query(5,
+		"(S3E17 'Cocktails') Who gets stuck in a straight jacket?", [
+			"Michael",
+			"Dwight",
+			"Creed",
+			"Merideth"
+		],
+
+		"Michael"));
+
+	queryNum = randomQuestion().num;
+	$("ul").on("click", ".choice", function() {
 		btn = $(this);
-		if(btn.text() === queries[questionNums[questionNums.length-1]].answer){
-			questionNums.push(randomQuestion());
+
+		if (btn.text() === queries[queryNum].answer) {
+
 			timeLeft = startingTime;
-			$("#time").text(timeLeft);
-		}else{
+			time.text(timeLeft);
+			queryNum = randomQuestion().num;
+
+		} else {
+
 			timeLeft = 5;
-			$("#time").text(timeLeft);
-			$("#question").text("Incorrect! The right answer was '" + queries[questionNums[questionNums.length-1]].answer + "'");
-			$("#choices").empty().append("<img src='https://placehold.it/200' />")
+			time.text(timeLeft);
+			question.text("Incorrect! The right answer was '" + queries[queryNum].answer + "'");
+
+			apiURL = "http://api.giphy.com/v1/gifs/random?" +
+					 "api_key=oN5N5nfVB5JFrvXamobIf4S9TTbt6d3F&" +
+					 "tag=the+office&" +
+					 "limit=1";
+
+			$.get(apiURL).then(function(response){
+
+				choices.empty()
+					.append("<iframe src=" + response.data.embed_url + " frameborder='0'></iframe>");
+
+			})
+
 		}
 	});
 
